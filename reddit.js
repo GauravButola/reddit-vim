d412a1b5f8b89e55138d26ccd4759e53f6a3dd13
Gaurav Butola <gauravbutola@gmail.com>
2015-04-03 15:09:22 +0530

    Working focuse next/previous comment and comment toggle

--- reddit.js
+++ reddit.js
@@ -5,9 +5,11 @@ $('body').on('keypress', function(key) {
     switch(key.which) {
         case 106: // j = 106
             console.log('j');
+            focusedComment = focusNextComment();
             break;
         case 107: // k = 107
             console.log('k');
+            focusedComment = focusPreviousComment();
             break;
         case 111: // o = 111
             console.log('o');
@@ -15,3 +17,19 @@ $('body').on('keypress', function(key) {
             break;
     }
 });
+
+var focusNextComment = function(element) {
+  element = element || focusedComment;
+  // Go up the DOM and find the closest `.thing` (comment container)
+  var commentContainer = element.closest('.thing');
+  // Find the next sibling comment; focus and return it.
+  return commentContainer.nextAll('.thing:first').find('.entry .tagline .expand').first().focus();
+};
+
+var focusPreviousComment = function(element) {
+  element = element || focusedComment;
+  // Go up the DOM and find the closest `.thing` (comment container)
+  var commentContainer = element.closest('.thing');
+  // Find the previous sibling comment; focus and return it.
+  return commentContainer.prevAll('.thing:first').find('.entry .tagline .expand').first().focus();
+};
