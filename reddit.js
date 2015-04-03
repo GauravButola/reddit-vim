ff9ad017ac544b7733f916b28b3a613ff24f75ea
Gaurav Butola <gauravbutola@gmail.com>
2015-04-03 16:04:02 +0530

    Separate toggle element selector from comment selector

--- reddit.js
+++ reddit.js
@@ -11,25 +11,38 @@ $('body').on('keypress', function(key) {
             console.log('k');
             focusedComment = focusPreviousComment();
             break;
+        case 98: // b = 98
+            console.log('b');
+            // TODO
+            break;
+        case 119: // w = 119
+            console.log('w');
+            // TODO
+            break;
         case 111: // o = 111
             console.log('o');
             focusedComment.click();
             break;
+        default:
+          console.log(key.which);
     }
 });
 
+var focusCommentToggleElement = function(commentContainer) {
+   /*
+    * [-] [+] thing
+    */
+   return commentContainer.find('.entry .tagline .expand').first().focus();
+};
+
 var focusNextComment = function(element) {
   element = element || focusedComment;
-  // Go up the DOM and find the closest `.thing` (comment container)
-  var commentContainer = element.closest('.thing');
-  // Find the next sibling comment; focus and return it.
-  return commentContainer.nextAll('.thing:first').find('.entry .tagline .expand').first().focus();
+  var nextCommentContainer = element.closest('.thing').nextAll('.thing:first');
+  return focusCommentToggleElement(nextCommentContainer);
 };
 
 var focusPreviousComment = function(element) {
   element = element || focusedComment;
-  // Go up the DOM and find the closest `.thing` (comment container)
-  var commentContainer = element.closest('.thing');
-  // Find the previous sibling comment; focus and return it.
-  return commentContainer.prevAll('.thing:first').find('.entry .tagline .expand').first().focus();
+  var previousCommentContainer = element.closest('.thing').prevAll('.thing:first');
+  return focusCommentToggleElement(previousCommentContainer);
 };
